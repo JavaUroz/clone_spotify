d609cc0eba0216155398df0e6234e3ab9230cb32
Javier Uroz <javier.uroz@accenture.com>
2024-11-22 09:54:12 -0300

    more tests

--- spotify/src/app/shared/directives/img-broken.directive.spec.ts
+++ spotify/src/app/shared/directives/img-broken.directive.spec.ts
@@ -1,10 +1,59 @@
-import { ElementRef } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 import { ImgBrokenDirective } from './img-broken.directive';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+
+//Componente de prueba
+@Component({
+  template:'<img appImgBroken [src]="srcMock" class="testing-directive">'
+})
+class TestComponent {
+  public srcMock: any = null
+}
 
 describe('ImgBrokenDirective', () => {
+  let component: TestComponent
+  let fixture: ComponentFixture<TestComponent>
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations:[
+        TestComponent,
+        ImgBrokenDirective
+      ]
+    })
+
+    fixture = TestBed.createComponent(TestComponent)
+    fixture.detectChanges()
+    component = fixture.componentInstance
+  })
+
   it('should create an instance', () => {
     const mockElement = new ElementRef('')
     const directive = new ImgBrokenDirective(mockElement);
     expect(directive).toBeTruthy();
   });
+
+  it('TestComponent should be instanced correctly', () => {
+    expect(component).toBeTruthy
+  })
+
+  it('Directive should change the image broken to default image base64', (done: DoneFn) => {
+    //Arrange
+    const beforeImgElement = fixture.debugElement.query(By.css('.testing-directive')).nativeElement
+    const beforeImgSrc = beforeImgElement.src
+    component.srcMock = undefined
+
+    setTimeout(() => {
+      const afterImgElement = fixture.debugElement.query(By.css('.testing-directive')).nativeElement
+      const afterImgSrc = afterImgElement.src
+
+      expect(afterImgSrc).toMatch(/\bdata:image\b/)
+      done()
+    }, 3000)
+
+    //Act
+
+    //Assert
+  })
 });
--- spotify/src/app/shared/pipe/order-list.pipe.ts
+++ spotify/src/app/shared/pipe/order-list.pipe.ts
@@ -8,25 +8,20 @@ import { TracksModel } from '@core/models/tracks.model';
 export class OrderListPipe implements PipeTransform {
 
   transform(value: Array<any>, args: string | null = null, sort: string = 'asc'): TracksModel[] {
-    try {
-      if(!args){
-        return value
-      } else {
-        const tmpList = value.sort((a, b) => {
-          if (a[args] < b[args]){
-            return -1
-          } else if (a[args] === b[args]){
-            return 0
-          } else if (a[args] > b[args]){
-            return 1
-          }
+    if(!args){
+      return value
+    } else {
+      const tmpList = value.sort((a, b) => {
+        if (a[args] < b[args]){
+          return -1
+        } else if (a[args] === b[args]){
+          return 0
+        } else if (a[args] > b[args]){
           return 1
-        })
-        return (sort === 'asc') ? tmpList : tmpList.reverse()
-      }
-    } catch (e) {
-      console.log('Algo salió mal:\n',e)
-      return value;
-    }    
+        }
+        return 1
+      })
+      return (sort === 'asc') ? tmpList : tmpList.reverse()
+    }   
   }
 }
